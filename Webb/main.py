b7a256cb7e6e6d00a844de2a11eed9c0c1e6264e
moasvennesson <62596297+moasvennesson@users.noreply.github.com>
2020-04-08 14:44:56 +0200

    clean-up

--- Webb/main.py
+++
@@ -1,9 +0,0 @@
-from bottle import route, run, template, request, redirect, error, static_file
-from datetime import datetime, date
-
-@route("/")
-def show_homepage():
-    ''' Hemsidan '''
-    return template("index")
-
-run(host='localhost', port=8080, debug=True, reloader=True)
