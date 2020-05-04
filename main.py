bde0f15cc86824c36dad3da3a8d308fec8f6da72
EmelieKul <emelie.j.k@hotmail.com>
2020-05-04 13:07:17 +0200

    Ändrat till det bättre :)

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ abs_views_path = os.path.join(abs_app_dir_path, 'views')
 abs_static_path = os.path.join(abs_app_dir_path, 'static')
 TEMPLATE_PATH.insert(0, abs_views_path)
 
+inloggad = "None"
 
 @route('/static/<filename>')
 def server_static(filename):
@@ -18,56 +19,52 @@ def startpage():
     return template("startpage")
 
 
-@get("/")
-def login_page():
-       return template("index", msg="")
-
-
-@post("/")
+@route("/", method=["POST", "GET"]) 
 def login():
+    global inloggad
     ''' Loginsidan'''
     msg = ""
-   
-    email = getattr(request.forms, "email")
-    password = getattr(request.forms, "password")
-    conn = sqlite3.connect("woman-up.db")
-    c = conn.cursor()
-    #find_user = ("SELECT * FROM user WHERE email = ? and password = ?")
-    c.execute("SELECT * FROM user WHERE email = ? and password = ?",(email, password))
-    if c.fetchone():
-        redirect("/startpage") 
-    else:
-        msg = "Inkorrekt email eller lösenord"
+    if request.method == "POST":
+        email = getattr(request.forms, "email")
+        password = getattr(request.forms, "password")
+        conn = sqlite3.connect("woman-up.db")
+        c = conn.cursor()
+        #find_user = ("SELECT * FROM user WHERE email = ? and password = ?")
+        c.execute("SELECT * FROM user WHERE email = ? and password = ?",(email, password))
+        if c.fetchone():
+            inloggad = email
+            print(inloggad)
+            redirect("/startpage") 
+        else:
+            msg = "Inkorrekt email eller lösenord"
 
     return template("index", msg=msg)
 
 
-@get("/register")
-def register_page():
-        return template("register", msg="")
 
 
-@post('/register')
+
+@route('/register', method=["POST", "GET"])
 def register():
     msg = ""
-    firstname = getattr(request.forms, 'firstname')
-    lastname = getattr(request.forms, 'lastname')
-    phonenumber = getattr(request.forms, 'phonenumber')
-    password = getattr(request.forms, 'password')
-    email = getattr(request.forms, 'email')
-    conn = sqlite3.connect('woman-up.db')
-    c = conn.cursor()
-  
-    c.execute('SELECT * FROM user WHERE email = ?', (email,))
-    if c.fetchone():
-         msg = "Den email adressen är reddan registrerad"
-    elif not password or not email:
-         msg = "Vänligen uppge all uppgifter"
-    else:
-        c.execute('INSERT INTO user VALUES(?,?,?,?,?,?,?,?)',(firstname, lastname, phonenumber, password, email, 0, 0, 0))
-        conn.commit()
-        redirect('/')
-
+    if request.method =="POST":
+        firstname = getattr(request.forms, 'firstname')
+        lastname = getattr(request.forms, 'lastname')
+        phonenumber = getattr(request.forms, 'phonenumber')
+        password = getattr(request.forms, 'password')
+        email = getattr(request.forms, 'email')
+        conn = sqlite3.connect('woman-up.db')
+        c = conn.cursor()
+    
+        c.execute('SELECT * FROM user WHERE email = ?', (email,))
+        if c.fetchone():
+            msg = "Den email adressen är reddan registrerad"
+        elif not password or not email:
+            msg = "Vänligen uppge all uppgifter"
+        else:
+            c.execute('INSERT INTO user VALUES(?,?,?,?,?,?,?,?)',(firstname, lastname, phonenumber, password, email, None, None, None))
+            conn.commit()
+            redirect('/')
 
     return template('register', msg=msg)
 
@@ -77,33 +74,44 @@ def popup():
     return template("FullPrivacyPolicy")
 
 
-@route('/map')
-def map():
+@route('/map', method=["POST", "GET"])
+def map(): 
+    global inloggad
+    print(inloggad)
     conn = sqlite3.connect("woman-up.db")
     cursor= conn.cursor()
-    inloggad = "234444433" ##den som är inloggad
+    inloggad = "" ##den som är inloggad
+    klart = ""
+    if request.method == 'POST':
+        print("Post")
+         ### den som är inloggad
+        all_location = getattr(request.forms, 'spara_plats') ## ser ut long,lat,offset
+        list_location = all_location.split(",") ##delar upp i array
+        print(list_location)
+        sql = "UPDATE user SET long ="+list_location[0]  +", lat =" + list_location[1]+ ", offset = "+list_location[2]+ " WHERE email = '"+ inloggad +"'"
+        print("ok")
+        klart = "Din plats är uppdaterad"
+        cursor.execute(sql)
+        
     location=[] #alla som ska skrivas ut på kartan sparas här
-    cursor.execute("select first_name,long,lat,offset from user where pnr != " +inloggad)
+    sql = "select first_name,long,lat,offset from user where email != '" + inloggad+"'"
+    print(sql)
+    cursor.execute(sql)
     for x in cursor:
         location.append(x)
     print(location)
     conn.commit()
 
-    return template('map',location=location)   
+    return template('map',location=location, klart=klart)
 
-@route('/save_L', method="POST")
-def save_L():
-    conn = sqlite3.connect("woman-up.db")
-    cursor= conn.cursor()
-    inloggad = "234444433" ### den som är inloggad
+@route('/emergency')
+def emergency():
+    return template('emergency')
 
-    all_location = getattr(request.forms, "spara_plats") ## ser ut long,lat,offset
-    list_location = all_location.split(",") ##delar upp i array
-    print(list_location)
-    sql = "UPDATE user SET long ="+list_location[0]  +", lat =" + list_location[1]+ ", offset = "+list_location[2]+ " WHERE pnr = "+ inloggad
-    cursor.execute(sql)
-    conn.commit()
-    redirect('map')
+
+@route('/chatt')
+def chatt():
+    return template('chatt')
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
---
+++ static/leaflet.js
@@ -0,0 +1,103 @@
+//skriver ut postion variablen
+var x = document.getElementById("DinPlats"), latitude,  longitude, accruacy;
+//används till att ge färger till vännerna.
+var Colors = ["Red","Orange","Yellow","Cyan","Blue","Purple","Pink","White","Gray","Brown"];
+document.getElementById("spara_plats").value = "test";
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+var mymap
+function showPosition(position) {
+  //sparar din plats i var latitude, longityde och accruacy
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  accruacy = position.coords.accuracy; 
+  document.getElementById("spara_plats").value = (latitude + "," + longitude + "," + accruacy);
+  //skriver till var x
+  x.innerHTML = latitude + " " + longitude + " accruacy: inom "+ accruacy + "m från din riktiga position";
+  getMap();
+}
+
+function getMap(){
+    //kart objektet
+    mymap = L.map('mapid').setView([latitude, longitude], 13);
+    //Genererar en karta layer
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+    L.marker([latitude,longitude]).addTo(mymap).bindPopup("<b>Din plats </b>").openPopup();
+    //L.marker ger en pin på platsen (på din plats)
+    var circle = L.circle([latitude, longitude], {
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.5,
+        radius: accruacy
+    }).addTo(mymap);
+   
+ 
+ 
+    getDatabaseFriendsOnMap(mymap);
+}
+ 
+//lägger till vänner på kartan
+function getFriendsMap(FriendsName,poslat,poslon,Offset,mymap){
+  for (i = 0; i < FriendsName.length-1; i++) {
+      if(FriendsName[i] != null &&  poslat!= null && poslon != null){
+      // använder Colors array för att ge färg till Personer L.marker ger en pin på platsen
+        L.marker([poslat[i],poslon[i]]).addTo(mymap).bindPopup("<b>"+FriendsName[i]+"</b>");
+        var circle = L.circle([poslat[i], poslon[i]], {
+            color: 'Blue',
+            fillColor: 'Purple',
+            fillOpacity: 0.5,
+            radius: Offset[i]
+        }).addTo(mymap);
+    }
+  }
+ 
+
+}
+ 
+//skriv ut de som är på karta
+ function printKarta(){
+    var printContents = document.getElementById("mapid").innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+ 
+}
+//Hämtar alla värden och lägger en pin på de
+function getDatabaseFriendsOnMap(mymap){
+    var stringtoarray;
+    var NamnF = [];
+    var poslat= [];
+    var poslon= [];
+    var Offset= [];
+    //Hämtar input från databasen
+    //Formatet måste , mellan varje
+    stringtoarray = document.getElementById("SendName").innerHTML;
+    NamnF = stringtoarray.split(",");
+    stringtoarray = document.getElementById("SendLat").innerHTML;
+    poslat = stringtoarray.split(",");
+    stringtoarray = document.getElementById("SendLong").innerHTML;
+    poslon = stringtoarray.split(",");
+    stringtoarray = document.getElementById("SendOffset").innerHTML;
+    Offset = stringtoarray.split(",");
+    ////////////////////////////////////////////////////////////////
+    // 3 arrayer latitud longitud Namn
+    getFriendsMap(NamnF,poslat,poslon,Offset,mymap);
+ 
+}
+
+getLocation();
