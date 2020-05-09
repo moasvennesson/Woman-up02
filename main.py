1d552e8dbc236799be71fff17030f0373c5557cd
EmilDavidsson <reqz93@gmail.com>
2020-05-09 15:28:56 +0200

    Revert "update"
    This reverts commit df62008fc6fcf89f9b7b9db6df695acc5023f6ec.

--- main.py
+++ main.py
@@ -7,47 +7,46 @@ abs_views_path = os.path.join(abs_app_dir_path, 'views')
 abs_static_path = os.path.join(abs_app_dir_path, 'static')
 TEMPLATE_PATH.insert(0, abs_views_path)
 
-inloggad = 'None'
+inloggad = "None"
 
 @route('/static/<filename>')
 def server_static(filename):
     return static_file(filename, root=abs_static_path)
 
 
-@route('/startpage')
+@route("/startpage")
 def startpage():
-    email = request.query.get('email')
-    return template('startpage', email = email)
+    email = request.query.get("email")
+    return template("startpage", email = email)
 
 
 @route("/", method=["POST", "GET"]) 
 def login():
-    ''' Loginsidan, hämtar email och password från HTML formuläret, 
-    ansluter till sqlite databasen woman-up och kollar om de uppgifterna finns'''
     global inloggad
+    ''' Loginsidan'''
     msg = ""
-    if request.method == 'POST':
-        email = getattr(request.forms, 'email')
-        password = getattr(request.forms, 'password')
-        conn = sqlite3.connect('woman-up.db')
+    if request.method == "POST":
+        email = getattr(request.forms, "email")
+        password = getattr(request.forms, "password")
+        conn = sqlite3.connect("woman-up.db")
         c = conn.cursor()
-        c.execute('SELECT * FROM user WHERE email = ? and password = ?',(email, password))
+        #find_user = ("SELECT * FROM user WHERE email = ? and password = ?")
+        c.execute("SELECT * FROM user WHERE email = ? and password = ?",(email, password))
         user = c.fetchone() 
         if user:
             inloggad = email
             print(inloggad)
-            redirect('/startpage?email={}'.format(user[4])) 
+            redirect("/startpage?email={}".format(user[4])) 
         else:
-            msg = 'Inkorrekt email eller lösenord'
+            msg = "Inkorrekt email eller lösenord"
 
-    return template('index', msg=msg)
+    return template("index", msg=msg)
 
 
-@route('/register', method=['POST', 'GET'])
+@route('/register', method=["POST", "GET"])
 def register():
-    '''Registrerar en ny användare i databasen och kollar om mailen redan är registrerad'''
     msg = ""
-    if request.method =='POST':
+    if request.method =="POST":
         firstname = getattr(request.forms, 'firstname')
         lastname = getattr(request.forms, 'lastname')
         phonenumber = getattr(request.forms, 'phonenumber')
@@ -58,9 +57,9 @@ def register():
     
         c.execute('SELECT * FROM user WHERE email = ?', (email,))
         if c.fetchone():
-            msg = 'Den email adressen är redan registrerad'
+            msg = "Den email adressen är reddan registrerad"
         elif not password or not email:
-            msg = 'Vänligen uppge all uppgifter'
+            msg = "Vänligen uppge all uppgifter"
         else:
             c.execute('INSERT INTO user VALUES(?,?,?,?,?,?,?,?)',(firstname, lastname, phonenumber, password, email, None, None, None))
             conn.commit()
@@ -69,7 +68,7 @@ def register():
     return template('register', msg=msg)
 
 
-@route('/FullPrivacyPolicy')
+@route("/FullPrivacyPolicy")
 def popup():
     return template("FullPrivacyPolicy")
 
@@ -117,8 +116,7 @@ def PrivacyPolicy():
 
 @route('/chatt')
 def chatt():
-    email = request.query.get('email')
-    return template('chatt', email = email)
+    return template('chatt')
 
 
 run(host='localhost', port=8083, debug=True, reloader=True)
